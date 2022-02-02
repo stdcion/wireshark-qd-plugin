95b4fb68b829fa85d3760e806a82103de3e85c9f
Konstantin Ivaschenko <stdcion@gmail.com>
2022-02-02 11:55:21 +0300

    Fix Doxygen comments in dbg.lua

--- qd_proto/dbg.lua
+++ qd_proto/dbg.lua
@@ -1,51 +1,51 @@
 -- @file dbg.lua
--- @brief Provides debug output functionality.
+-- @brief Provides debug output.
 package.prepend_path(Dir.global_plugins_path())
 local utils = require("qd_proto.utils")
 
 local dbg = {}
 
--- Print message with file, line and severity.
--- @param file Path to file.
--- @param line Line in file.
--- @param str Message.
+-- Prints a message containing the file, line and severity.
+-- @param file The path to the file.
+-- @param line The line in the file.
+-- @param str The message.
 local function dprint(file, line, str)
     file = utils.get_filename(file)
     print("QD " .. "File: " .. file .. " " .. "Line: " .. line .. " " .. str)
 end
 
--- Print trace.
--- @param file Path to file.
--- @param line Line in file.
--- @param str Trace message.
+-- Prints a trace message.
+-- @param file The path to the file.
+-- @param line The line in the file.
+-- @param str The trace message.
 function dbg.trace(file, line, str) dprint(file, line, "Trace: " .. str) end
 
--- Print info.
+-- Prints an information message.
 -- @param file Path to file.
--- @param line Line in file.
--- @param str Info message.
+-- @param line The line in the file.
+-- @param str The info message.
 function dbg.info(file, line, str) dprint(file, line, "Info: " .. str) end
 
--- Print warning.
+-- Prints a warning message.
 -- @param file Path to file.
--- @param line Line in file.
--- @param str Warning message.
+-- @param line The line in the file.
+-- @param str The warning message.
 function dbg.warn(file, line, str) dprint(file, line, "Warn: " .. str) end
 
--- Print error.
--- @param file Path to file.
--- @param line Line in file.
--- @param str Error message.
+-- Prints an error message.
+-- @param file The path to the file.
+-- @param line The line in the file.
+-- @param str The error message.
 function dbg.error(file, line, str) dprint(file, line, "Error: " .. str) end
 
--- Get full path to file where call this function.
--- Analogue C maros __FILE__.
--- @return Path to file.
+-- Gets the full path to the file in which this function is called.
+-- @note Analogue C maros __FILE__.
+-- @return The path to the file.
 function dbg.file() return debug.getinfo(2, 'S').source end
 
--- Get line in file where call this function.
--- Analogue C maros __LINE__.
--- @return Line.
+-- Gets the line in the file where this function is called.
+-- @note Analogue C maros __LINE__.
+-- @return The line in the file.
 function dbg.line() return debug.getinfo(2, 'l').currentline end
 
 return dbg
