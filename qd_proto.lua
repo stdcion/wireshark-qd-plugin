5fb76138c1b848c8adbe3c16221119d5da115ca3
Konstantin Ivaschenko <stdcion@gmail.com>
2022-02-02 14:25:02 +0300

    Fix Doxygen comments in qd_proto.lua

--- qd_proto.lua
+++ qd_proto.lua
@@ -1,12 +1,14 @@
+-- @file qd_proto.lua
+-- @brief Main plugin file. Contains functions that Wireshark calls.
 package.prepend_path(Dir.global_plugins_path())
 local settings = require("qd_proto.settings")
 local utils = require("qd_proto.utils")
 local qd = require("qd_proto.dissectors.qd")
 local heartbeat = require("qd_proto.dissectors.heartbeat")
 
--- Create protocol object.
+-- Creates the protocol object.
 local qd_proto = Proto("QD", "Quote Distribution protocol")
--- Register fields for Wireshark.
+-- Registers the fields for Wireshark.
 utils.append_to_table(qd_proto.fields, qd.fields)
 utils.append_to_table(qd_proto.fields, heartbeat.fields)
 
@@ -41,13 +43,12 @@ function qd_proto.prefs_changed()
     end
 end
 
--- Parse QD message.
--- @param proto Protocol object.
--- @param type QD message type.
--- @param tvb_buf Input buffer.
--- @param packet_info Packet information.
--- @param subtree Subtree for display fields in Wireshark
---                (for concrete type message).
+-- Parses QD message.
+-- @param proto The protocol object.
+-- @param type The type of QD message.
+-- @param tvb_buf The input buffer.
+-- @param packet_info The packet information.
+-- @param subtree The subtree for display fields in Wireshark.
 local function parse_message(proto, type, tvb_buf, packet_info, subtree)
     if (type ~= nil and tvb_buf:len() ~= 0) then
         if (type.val_uint == qd.type.HEARTBEAT) then
@@ -57,15 +58,17 @@ local function parse_message(proto, type, tvb_buf, packet_info, subtree)
 end
 
 -- Called Wireshark when package captured.
--- @param tvb_buf Tvb represents the packet’s buffer. It is passed as an argument
---        to dissectors, and can be used to extract information (via TvbRange)
---        from the packet’s data.
--- @param packet_info Packet information.
--- @param tree TreeItem represent information in the packet details pane
+-- @param tvb_buf The Tvb represents the packet’s buffer.
+--                It is passed as an argument to dissectors,
+--                and can be used to extract information (via TvbRange)
+--                from the packet’s data.
+-- @param packet_info The packet information.
+-- @param tree The TreeItem represent information in the packet details pane
 --             of Wireshark, and the packet details view of TShark.
--- @return 0 - if the packet does not belong to your dissector,
---         tvb_buf:len() and set desegment_offset/desegment_len if needs more bytes,
---         tvb_buf:len() if don't need more bytes.
+-- @return 0             - if the packet does not belong to your dissector;
+--         tvb_buf:len() - and set desegment_offset/desegment_len
+--                         if needs more bytes;
+--         tvb_buf:len() - if don't need more bytes.
 function qd_proto.dissector(tvb_buf, packet_info, tree)
     -- Do not process empty packages.
     local len = tvb_buf:len()
@@ -73,16 +76,16 @@ function qd_proto.dissector(tvb_buf, packet_info, tree)
 
     local byte_processed = 0
     while byte_processed < len do
-        -- Dissect QD message.
+        -- Dissects the QD message.
         local result = qd.dissect(qd_proto, tvb_buf, byte_processed,
                                   packet_info, tree)
         if result.qd_full_msg_len > 0 then
             -- This is QD message.
             packet_info.cols.protocol = qd_proto.name
-            -- Parsing QD message.
+            -- Parses QD message.
             parse_message(qd_proto, result.qd_message.type,
-                            result.qd_message.data, packet_info, result.subtree)
-            -- Try find next QD message in this package.
+                          result.qd_message.data, packet_info, result.subtree)
+            -- Tries to find the next QD message in this packet.
             byte_processed = byte_processed + result.qd_full_msg_len
         elseif result.qd_full_msg_len == 0 then
             -- Ignoring package.
@@ -98,6 +101,6 @@ function qd_proto.dissector(tvb_buf, packet_info, tree)
             return len
         end
     end
-    -- Return count handled bytes.
+    -- Returns count handled bytes.
     return byte_processed
 end
