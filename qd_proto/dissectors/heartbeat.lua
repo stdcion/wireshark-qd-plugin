c94d4bd60ba5876432b467cb3cf5349695c63215
Konstantin Ivaschenko <stdcion@gmail.com>
2022-03-23 19:00:33 +0300

    Replace utils function to stream_reader

--- qd_proto/dissectors/heartbeat.lua
+++ qd_proto/dissectors/heartbeat.lua
@@ -2,7 +2,7 @@
 -- @brief The HEARTBEAT message dissector.
 package.prepend_path(Dir.global_plugins_path())
 local utils = require("qd_proto.utils")
-local dbg = require("qd_proto.dbg")
+local stream_reader = require("qd_proto.io.stream_reader")
 
 local heartbeat = {}
 
@@ -68,24 +68,17 @@ local function has_lag_mark(content_byte)
     return (bit.band(content_byte, content.LAG_MARK) ~= 0) and true or false
 end
 
--- Dissects the HEARTBEAT message.
--- @param proto The protocol object.
--- @param tvb_buf The input buffer.
--- @param packet_info The packet information.
--- @param subtree The tree for display fields in Wireshark.
-function heartbeat.dissect(proto, tvb_buf, packet_info, subtree)
-    local off = 0
+-- Displays HEARTBEAT message in Wireshark.
+-- @param stream Represents the input buffer.
+-- @param tree The tree for display.
+local function display(stream, tree)
     -- Handles a byte of content.
-    local content_range = tvb_buf(off, 1)
-    local content_byte = content_range:uint()
-    if (content_byte == nil) then
-        dbg.error(dbg.file(), dbg.line(), "Can't read content_byte.")
-    end
+    local content_byte, content_range = stream:read_uint8()
 
     local ws_fields = heartbeat.ws_fields
     -- Adds content subtree.
-    local content_flags_tree = subtree:add(ws_fields.content, content_range,
-                                           content_byte)
+    local content_flags_tree = tree:add(ws_fields.content, content_range,
+                                        content_byte)
     -- Adds content bit field to subtree.
     content_flags_tree:add(ws_fields.content_millis, content_range)
     content_flags_tree:add(ws_fields.content_time_mark, content_range)
@@ -95,59 +88,40 @@ function heartbeat.dissect(proto, tvb_buf, packet_info, subtree)
     if (is_empty_content(content_byte)) then return end
 
     -- Handles the fields.
-    off = off + 1
     if (has_time_millis(content_byte)) then
-        local time_millis = utils.read_compact_long(tvb_buf, off)
-        if (time_millis == nil) then
-            dbg.error(dbg.file(), dbg.line(), "Can't read time_millis.")
-            return
-        end
-        local time_millis_range = tvb_buf(off, time_millis.sizeof)
-        -- Displays in milliseconds.
-        subtree:add(ws_fields.time_mills, time_millis_range, time_millis.val)
-
-        -- Gets the time in second.
-        local sec = (time_millis.val / 1000):tonumber()
-        -- Gets the remainder in nanoseconds
-        local nanosec_remainder = (time_millis.val % 1000):tonumber() * 1000000
-        -- Displays the time in UTC.
-        local ns_time = NSTime(sec, nanosec_remainder)
-        subtree:add(ws_fields.time_utc, time_millis_range, ns_time)
-        off = time_millis.next_pos
+        local time_millis, time_millis_range = stream:read_compact_long()
+        local ns_time = utils.millis_to_nstime(time_millis)
+        tree:add(ws_fields.time_mills, time_millis_range, time_millis)
+        tree:add(ws_fields.time_utc, time_millis_range, ns_time)
     end
     if (has_time_mark(content_byte)) then
-        local time_mark = utils.read_compact_int(tvb_buf, off)
-        if (time_mark == nil) then
-            dbg.error(dbg.file(), dbg.line(), "Can't read time_mark.")
-            return
-        end
-        local time_mark_range = tvb_buf(off, time_mark.sizeof)
-        time_mark.val = utils.int_to_long(time_mark.val)
-        subtree:add(ws_fields.time_mark, time_mark_range, time_mark.val)
-        off = time_mark.next_pos
+        local time_mark, time_mark_range = stream:read_compact_int()
+        time_mark = utils.int_to_long(time_mark)
+        tree:add(ws_fields.time_mark, time_mark_range, time_mark)
     end
     if (has_delta_mark(content_byte)) then
-        local delta_mark = utils.read_compact_int(tvb_buf, off)
-        if (delta_mark == nil) then
-            dbg.error(dbg.file(), dbg.line(), "Can't read delta_mark.")
-            return
-        end
-        local delta_mark_range = tvb_buf(off, delta_mark.sizeof)
-        delta_mark.val = utils.int_to_long(delta_mark.val)
-        subtree:add(ws_fields.delta_mark, delta_mark_range, delta_mark.val)
-        off = delta_mark.next_pos
+        local delta_mark, delta_mark_range = stream:read_compact_int()
+        delta_mark = utils.int_to_long(delta_mark)
+        tree:add(ws_fields.delta_mark, delta_mark_range, delta_mark)
     end
     if (has_lag_mark(content_byte)) then
-        local lag_mark = utils.read_compact_int(tvb_buf, off)
-        if (lag_mark == nil) then
-            dbg.error(dbg.file(), dbg.line(), "Can't read lag_mark.")
-            return
-        end
-        local lag_mark_range = tvb_buf(off, lag_mark.sizeof)
-        lag_mark.val = utils.int_to_long(lag_mark.val)
-        subtree:add(ws_fields.lag_mark, lag_mark_range, lag_mark.val)
-        off = lag_mark.next_pos
+        local lag_mark, lag_mark_range = stream:read_compact_int()
+        lag_mark = utils.int_to_long(lag_mark)
+        tree:add(ws_fields.lag_mark, lag_mark_range, lag_mark)
     end
+end
+
+-- Dissects the HEARTBEAT message.
+-- @param proto The protocol object.
+-- @param tvb_buf The input buffer.
+-- @param packet_info The packet information.
+-- @param subtree The tree for display fields in Wireshark.
+function heartbeat.dissect(proto, tvb_buf, packet_info, subtree)
+    local res, err = pcall(function()
+        local sr = stream_reader:new(tvb_buf, 0)
+        while (sr:is_empty() ~= true) do display(sr, subtree) end
+    end)
+    if (res == false) then error(err) end
 end
 
 return heartbeat
